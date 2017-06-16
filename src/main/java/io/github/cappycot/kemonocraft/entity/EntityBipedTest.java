d9544b806742759c473ba2867769be40b3a75ea0
Cappycot <chriswang444@gmail.com>
2017-06-15 23:52:49 -0500

    Added Gray Wolf test model. It looks bad.

--- src/main/java/io/github/cappycot/kemonocraft/entity/EntityBipedTest.java
+++ src/main/java/io/github/cappycot/kemonocraft/entity/EntityBipedTest.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public class EntityBipedTest extends EntityTameable {
@@ -13,6 +14,7 @@ public class EntityBipedTest extends EntityTameable {
 		super(p_i1604_1_);
 		this.setSize(0.6F, 1.8F);
 		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
 		this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityLiving.class, 8.0F));
 	}
 	
